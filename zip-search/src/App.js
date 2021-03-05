fe2b505b98eef3280b281c0022235bdf33663d4f
Farhene Sultana <farhenesultana@gmail.com>
2021-03-05 14:28:03 -0500

    Added submit button. I am still trying to figure out how to make my submit button work with the fetch api call

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -12,15 +12,22 @@ function City(props) {
   );
 }
 
-function ZipSearchField( {onZipChange}) {
+function ZipSearchField( {onZipChange , onZipSubmit}) {
   return (
     <div>
-      <label>Zip Code: </label>
-      <input 
-        type = "text" 
+     <form style={{ display: "flex", margin: "50px"}} >
+      <h3> Zip code:  </h3>
+      <input
+        onChange={onZipChange}
+        type="text"
         placeholder = "Enter 5 digit Zipcode"
-        onChange={onZipChange} 
       />
+      <input 
+        type="submit" 
+        onSubmit={onZipSubmit} 
+        className="btn btn-secondary"
+      />
+    </form>
     </div>
   );
 }
@@ -30,29 +37,38 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      zipCode: '',
+      zipCode: "",
       cities: [],
     }
+
+    this.zipChanged = this.zipChanged.bind(this);
+    this.zipSubmit = this.zipSubmit.bind(this);
   }
 
   zipChanged(e) {
     //Make GET request for the zip resource
     // then, when you receive the result, store it in state
-    if(e.target.value <= 5){
-      this.setState({
-        zipCode: e.target.value
-      });
+    this.setState({
+      zipCode: e.target.value
+    });
 
+    console.log(this.state.zipCode);
+  }
+
+  zipSubmit(e) {
+    alert('You pressed submit for: ' + this.state.zipCode);
+    e.preventDefault();
+    if(this.state.zipCode.length === 5){
       fetch('http://ctp-zip-api.herokuapp.com/zip/' + this.state.zipCode)
-        .then(response => response.json)
-        .then(result => {
-            console.log(result["City"]);
-            this.setState({
-              cities: result
-            })
-          }
-        )
-      }
+      .then(response => response.json)
+      .then(result => {
+          console.log(result["City"]);
+          this.setState({
+            cities: result
+          })
+        }
+      )
+    }
   }
 
   render() {
@@ -61,17 +77,28 @@ class App extends Component {
     var city_array = [];
 
     for(var i = 0; i < cities_.length; i++){
-      console.log(cities_[i]);
+      //console.log(cities_[i]);
       city_array.push( <City city={cities_[i]["City"]} /> )
     }
     
+    const zipStyle = {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    };
+
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField onZipChange={ (e) => this.zipChanged(e)} />
-        <div>
+        <div style={zipStyle}>
+          <ZipSearchField 
+            onZipChange={ (e) => this.zipChanged(e)} 
+            onZipSubmit={ (e) => this.zipSubmit(e) }
+          />
+        </div>
+        <div style={zipStyle}>
           <h5> The Cities in the Following Zipcode are: </h5>
         </div>
         <div>
