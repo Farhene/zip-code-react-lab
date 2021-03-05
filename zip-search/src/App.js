23a16e52fd29df98cb5d99e82561e0204a8614d0
Farhene Sultana <farhenesultana@gmail.com>
2021-03-05 16:11:10 -0500

    I had a syntax error all along. But it works now finally!! I just have to fix up my design to make it look nicer!

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-function City({city}) {
-  //could I perhaps populate cities here? Maybe Ill do it in the App class
-  if(city.length > 0){
-    console.log("yes");
-  }
+function City(props) {
   return (
   <div>
     <ul>
-      <li>City name: {city}</li>
+      <li>City name: {props.city}</li>
     </ul>
   </div>
   );
@@ -18,21 +14,14 @@ function City({city}) {
 
 function ZipSearchField( {onZipChange /*, onZipSubmit*/}) {
   return (
-    <div>
-     <form style={{ display: "flex", margin: "50px"}} >
+    <div style={{marginTop: "20px"}}>
+     <form style={{display: "flex"}}>
       <h3> Zip code:  </h3>
       <input
         onChange={onZipChange}
         type="text"
         placeholder = "Enter 5 digit Zipcode"
       />
-      {/*
-      <input 
-        type="submit" 
-        onSubmit={onZipSubmit} 
-        className="btn btn-secondary"
-      />
-      */}
     </form>
     </div>
   );
@@ -53,56 +42,35 @@ class App extends Component {
   zipChanged(e) {
     //Make GET request for the zip resource
     // then, when you receive the result, store it in state
-    this.setState({
-      zipCode: e.target.value
-    });
+    let zip = e.target.value;
 
-    if(e.target.value.length === 5){
+    if(zip.length === 5){
       console.log(e.target.value);
-      fetch(`http://ctp-zip-api.herokuapp.com/zip/${e.target.value}`)
-      .then(response => response.json)
+      fetch(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+      .then(response => response.json())
       .then(result => {
             this.setState({
-              'cities': result
+              cities: result
             });
-          //console.log("finished loading cities");
-          //console.log(result); 
-          //shows I got the json file which is put into the cities array
+          console.log(result); 
         },
         
       )
        .catch( err => {
          console.log(`Failed: ${err}`)
       })
-    }
-    
-  }
+    }    
 
-  /*
-  zipSubmit(e) {
-    alert('You pressed submit for: ' + this.state.zipCode);
-    e.preventDefault();
-    if(this.state.zipCode.length === 5){
-      fetch('http://ctp-zip-api.herokuapp.com/zip/' + this.state.zipCode)
-      .then(response => response.json)
-      .then(result => {
-          this.setState({
-            cities: result
-          })
-        }
-      )
-      .catch( err => {
-        console.log(`Failed: ${err}`)
-      })
-    }
-    return;
+    this.setState({
+      zipCode: zip
+    }); //we could use callback function here to ensure updated value
+
   }
-  */
 
   render() {
     // here I can populate cities into an array
     //the cities doesnt seem to be fetching properly
-   // console.log(this.state.cities.length + " Length of city array");
+    console.log(this.state.cities.length + " Length of city array");
     const cities_ = this.state.cities;
     var city_array = [];
 
@@ -125,19 +93,17 @@ class App extends Component {
         <div style={zipStyle}>
           <ZipSearchField 
             onZipChange={ (e) => this.zipChanged(e)} 
-            //onZipSubmit={ (e) => this.zipSubmit(e) }
           />
         </div>
         <div style={zipStyle}>
           <h5> The Cities in the Following Zipcode are: </h5>
         </div>
         <div>
-          <City city={this.state.cities} />
-            {/* 
+            {
               city_array.length === 0 
               ? <div style={zipStyle}> No cities found </div>
-              : <div style={zipStyle}> city_array </div>
-            */}        
+              : <div style={zipStyle}> {city_array} </div>
+            }        
         </div>
       </div>
     );
